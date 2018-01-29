2f43f397b9e452c7016182760a20fadc87619b63
kilyig <yigit@kilicoglu.nom.tr>
2018-01-30 00:49:28 +0300

    Added drive Method for the Arm

--- src/org/usfirst/frc/team6014/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team6014/robot/subsystems/Arm.java
@@ -8,6 +8,7 @@
 package org.usfirst.frc.team6014.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * A subsystem for controlling the arm and its components.
@@ -22,24 +23,26 @@ public class Arm extends Subsystem {
 	
 	SpeedController leftArm = RobotMap.leftArmEnum.generate(RobotMap.leftArm);
 	SpeedController rightArm = RobotMap.rightArmEnum.generate(RobotMap.rightArm);	
-	SpeedControllerGroup arm = new SpeedControllerGroup(leftArm,rightArm);
+	DifferentialDrive arm = new DifferentialDrive(leftArm,rightArm);
 	
 	Encoder hingeEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
-	hingeEncoder.setMaxPeriod(0.1); //ayarlanabilir
-	hingeEncoder.reset();
+	//hingeEncoder.reset();
 	
 	double countsPerRevolution = 497;
 	double angularRange = 180;
-	boolean overrotated = false;
-	
-	if(!hingeEncoder.getStopped() && hingeEncoder.overrotated()){
-		arm.set(0); //set speed to 0
-	}
 
 	@Override
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
+	}
+	
+	public void arcadeDrive(double speed, double rotation) {
+		if(arm.overrotated()){
+			arm.arcadeDrive(0,0);
+		}else{
+			arm.arcadeDrive(speed,rotation);
+		}
 	}
 	
 	public static boolean overrotated(){
