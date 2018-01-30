f0278d2fe481754a50c8ce8e16e05fb38f5e994f
kilyig <yigit@kilicoglu.nom.tr>
2018-01-30 10:56:50 +0300

    Changed the drive method for Arm.

--- src/org/usfirst/frc/team6014/robot/Teleop.java
+++ src/org/usfirst/frc/team6014/robot/Teleop.java
@@ -7,7 +7,6 @@ public class Teleop {
 	public static void periodic() {
 		Robot.drive.arcadeDrive(-Robot.oi.getRawY()*Robot.oi.getReverseFactor(),Robot.oi.getRawX());
 		Robot.drive.setMaxOutput((-Robot.oi.getRawOtherY()+1)/2);
-		Robot.arm.arcadeDrive(speed, rotation);
-		// TODO place double getLeftTrigger() - getRightTrigger()
+		Robot.Arm.set(getLeftTrigger() - getRightTrigger());
 	}
 }
