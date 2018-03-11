e818d768f90b69b6e94ad2d4ff81999c89fc20a0
Ege <Ege@EGE-LAPTOP>
2018-03-12 00:59:42 +0300

    Auto Scale Command Groups implemented

--- src/org/usfirst/frc/team6014/robot/autonomous/commandgroups/AutoScaleDecisionTreeLeft.java
+++ src/org/usfirst/frc/team6014/robot/autonomous/commandgroups/AutoScaleDecisionTreeLeft.java
@@ -10,8 +10,13 @@ package org.usfirst.frc.team6014.robot.autonomous.commandgroups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 import org.usfirst.frc.team6014.robot.Robot;
+import org.usfirst.frc.team6014.robot.autonomous.commands.Delay;
+import org.usfirst.frc.team6014.robot.autonomous.commands.DistanceStraightDrive;
+import org.usfirst.frc.team6014.robot.autonomous.commands.Launch;
 import org.usfirst.frc.team6014.robot.autonomous.commands.MechanismTurn;
+import org.usfirst.frc.team6014.robot.autonomous.commands.RotateArm;
 import org.usfirst.frc.team6014.robot.autonomous.commands.TimedStraightDrive;
+import org.usfirst.frc.team6014.robot.autonomous.commands.TurnToAngle;
 
 /**
  * This commandgroup is responsible for choosing a the set of behaviors of the robot according to the starting position
@@ -19,29 +24,46 @@ import org.usfirst.frc.team6014.robot.autonomous.commands.TimedStraightDrive;
 public class AutoScaleDecisionTreeLeft extends CommandGroup {
 	public AutoScaleDecisionTreeLeft() {
 		addSequential(new MechanismTurn(0.8, -0.4));
-		String gameSwitchData = Robot.gameData.substring(0,2);
+		String gameSwitchData = Robot.gameData.substring(1,2);
 		if(gameSwitchData.length() > 0)
         {
-			if(gameSwitchData.equals("RR"))
+			if(gameSwitchData.equals("R"))
 			{
-				//If first switch and scale are at right side
+				addSequential(new DistanceStraightDrive(6.3,0.8,0));
+				addSequential(new TurnToAngle(90,0.7,1.5));
+				addSequential(new DistanceStraightDrive(6.3,0.8,90));
+				addSequential(new TurnToAngle(0,0.7,1.5));
+				addSequential(new DistanceStraightDrive(1.6,0.8,0));
+				addSequential(new TurnToAngle(-90,0.7,1.5));
+				addParallel(new RotateArm(180));
+				addSequential(new Delay(2.0));
+				addSequential(new Launch(0.5));
+				addParallel(new RotateArm(110));
+				addSequential(new Delay(1.0));
+				addParallel(new RotateArm(60));
+				addSequential(new Delay(1.0));
+				addParallel(new RotateArm(10));
+				addSequential(new Delay(1.0));
 			}
-			else if(gameSwitchData.equals("LL"))
+			else if(gameSwitchData.equals("L"))
 			{
-				//If first switch and scale are at left side
-			}
-			else if(gameSwitchData.equals("LR"))
-			{
-				//If first switch is at left and the scale is at right side
-			}
-			else if(gameSwitchData.equals("RL"))
-			{
-				//If first switch is at right and scale is at left side
+				addSequential(new DistanceStraightDrive(8.20,0.8,0));
+				addSequential(new TurnToAngle(90,0.7,1.5));
+				addParallel(new RotateArm(180));
+				addSequential(new Delay(2.0));
+				addSequential(new Launch(0.5));
+				addParallel(new RotateArm(110));
+				addSequential(new Delay(1.0));
+				addParallel(new RotateArm(60));
+				addSequential(new Delay(1.0));
+				addParallel(new RotateArm(10));
+				addSequential(new Delay(1.0));
 			}
 			else
 			{
 				//There's goddamn error m8
 				System.out.println("NO GAME DATA");
+				addSequential(new DistanceStraightDrive(3-Robot.robotLength,0.8,0));
 			}
 			
 		}
