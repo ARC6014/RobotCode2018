875fec487f2a9014c415446eb1e539cc3776e782
VVanguard <bkaymakcioglu@gmail.com>
2018-09-08 14:41:39 +0300

    change AutoDesicionSwitchTreeLeft(Experimental)

--- src/main/java/frc/team6014/robot/autonomous/commandgroups/AutoSwitchDecisionTreeLeft.java
+++ src/main/java/frc/team6014/robot/autonomous/commandgroups/AutoSwitchDecisionTreeLeft.java
@@ -16,6 +16,8 @@ import frc.team6014.robot.autonomous.commands.Launch;
 import frc.team6014.robot.autonomous.commands.RotateArm;
 import frc.team6014.robot.autonomous.commands.TimedStraightDrive;
 import frc.team6014.robot.autonomous.commands.TurnToAngle;
+import frc.team6014.robot.autonomous.commands.DropArm;
+import frc.team6014.robot.autonomous.commands.Intake;
 
 /**
  * This commandgroup is responsible for choosing a the set of behaviors of the robot according to the starting position
@@ -30,8 +32,25 @@ public class AutoSwitchDecisionTreeLeft extends CommandGroup {
 			if(gameSwitchData.equals("R"))
 			{
 				//If first switch and scale are at right side
-				addSequential(new TimedStraightDrive(0.8,-0.8,0));
-				
+				addSequential(new TurnToAngle(90,-0.6,2));
+				addSequential(new TimedStraightDrive(0.5,-0.8,0));
+				addSequential(new TurnToAngle(0,-0.6,2));
+				addSequential(new TimedStraightDrive(1.7,-0.8,0));
+				addSequential(new TurnToAngle(-90,-0.6,2));
+				addSequential(new TimedStraightDrive(1.6,-0.8,0));
+				addSequential(new TurnToAngle(-90,-0.6,2));
+				addSequential(new TimedStraightDrive(0.4,-0.8,0));
+				addSequential(new Launch(0.6));
+
+				//Taking another box
+				//addSequential(new TimedStraightDrive(0.4,0.8,0));
+				//addSequential(new TurnToAngle(-90,0.6,2));
+				//addSequential(new DropArm(0.2,0.5));
+				//addSequential(new TimedStraightDrive(0.4,-0.8,90));
+				//addSequential(new Intake(1));
+				//What it does is drops the first box the takes another one from where our switch is
+
+
 			}
 			else if(gameSwitchData.equals("L"))
 			{
@@ -39,6 +58,18 @@ public class AutoSwitchDecisionTreeLeft extends CommandGroup {
 				addSequential(new TurnToAngle(90,-0.6,2));
 				addSequential(new TimedStraightDrive(0.4,-0.8,90));
 				addSequential(new Launch(0.6));
+
+				//Taking another box
+				//addSequential(new TimedStraightDrive(0.4,0.8,90));
+				//addSequential(new TimedStraightDrive(0.5,-0.8,0));
+				//addSequential(new TurnToAngle(0,-0.6,2));
+				//addSequential(new TimedStraightDrive(1.7,-0.8,0));
+				//addSequential(new TurnToAngle(-90,-0.6,2));
+				//addSequential(new TimedStraightDrive(0.5,-0.4,0));
+				//addSequential(new TurnToAngle(-90,-0.6,2));
+				//addSequential(new TimedStraightDrive(0.4,-0.8,90));
+				//addSequential(new Intake(1));
+				//What it does is drops the first box the takes another one from where our switch is
 			}
 			else
 			{
