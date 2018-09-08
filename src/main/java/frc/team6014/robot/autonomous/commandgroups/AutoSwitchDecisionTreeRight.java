a2077a4369f5f6f494469cfc7fc96ec5c397dc21
guvkay20 <guvkay.20@robcol.k12.tr>
2018-09-08 14:32:48 +0300

    Updated autonomous for right position left switch

--- src/main/java/frc/team6014/robot/autonomous/commandgroups/AutoSwitchDecisionTreeRight.java
+++ src/main/java/frc/team6014/robot/autonomous/commandgroups/AutoSwitchDecisionTreeRight.java
@@ -28,6 +28,7 @@ public class AutoSwitchDecisionTreeRight extends CommandGroup {
         {
 			if(gameSwitchData.equals("R"))
 			{
+				// Is this not good already?
 				//If first switch at right side
 				addSequential(new TurnToAngle(-90,-0.6,2));
 				addSequential(new TimedStraightDrive(0.4,-0.8,-90));
@@ -38,7 +39,11 @@ public class AutoSwitchDecisionTreeRight extends CommandGroup {
 			{
 				//If first switch at left side
 				addSequential(new TimedStraightDrive(0.8,-0.8,0));
-				
+				addSequential(new TurnToAngle(-90,-0.6,2));
+				addSequential(new TimedStraightDrive(1.6, -0.8,-90));
+				addSequential(new TurnToAngle(180, -0.6, 2));
+				addSequential(new TimedStraightDrive(1.6, -0.8, 180));
+				addSequential(new Launch(0.6));
 			}
 			else
 			{
